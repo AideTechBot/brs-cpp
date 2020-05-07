8fd4822dd79d3f9f8960288719dcd83670cd8499
aidetechbot <mandio0302@gmail.com>
2020-05-06 22:49:46 -0400

    Initial Commit

---
+++ brs/brs.cpp
@@ -0,0 +1,19 @@
+﻿// brs.cpp : Defines the entry point for the application.
+//
+
+#define BRS_BRICKADIA_IMPLEMENTATION_H
+#include "brs.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	Color r(255, 0, 0, 0);
+	Color g(0, 255, 0, 0);
+	Color b(0, 0, 255, 0);
+	Color a(0, 0, 0, 255);
+	cout << r + b + g + a;
+	return 0;
+}
